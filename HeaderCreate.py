179d8f0fae82a59140e4d43d91c2a784667e390c
Anthony Tam <25327116+anthonytwh@users.noreply.github.com>
2018-02-28 00:24:12 -0500

    Code Cleanup

--- HeaderCreate.py
+++ HeaderCreate.py
@@ -41,7 +41,7 @@ def headers_basic(data):
 			- If any element in input list is not found in output list, it is added to output list based on the index of the element. 
 			
 	"""
-# 
+
 	inList = data
 	outList = []
 	for element in inList:
@@ -54,7 +54,7 @@ def headers_basic(data):
 
 def headers_traverse(data):
 	"""
-		Traverse through JSON dataset to extract headers in all levels (including headers of lists/dicts).
+		Traverse through JSON dataset to extract headers in all levels (including headers of lists/dicts). In this example, all nested JSON elements are type:dict.
 
 		Method:
 			- Recursively loop through JSON data from higher level and if a nested element is found, loop through nested element for headers and add all found headers sequentially into new list.
@@ -68,41 +68,15 @@ def headers_traverse(data):
 	"""
 
 	if isinstance(data, list):
-		# print ("list")
 		return [headers_traverse(v) for i, v in enumerate(data)]
 
 	elif isinstance(data, dict):
-		# print ("dict")
-		# print (list(data.keys()))
-		# Emptylist.append(list(data.keys()))
-
 		for key in data.keys():
 			if not any(key == header for header in Emptylist):
-				# print('append: ' + key)
 				Emptylist.append(key)
-
 			for k, v in data.items():
 				if ((key == k) and ((type(v) == list) or (type(v) == dict))):
-					# print (key, k, v, type(v))
 					headers_traverse(v)
-
-				# if ((key == k) and ((type(v) == list) or (type(v) == dict))):
-				# 	print (key, k, v, type(v))
-					# if type(v) == dict:
-					# 	print('append: ' + k)
-					# 	Emptylist.append(k)
-
-				# if (type(v) == list or dict and key == k and not(type(v) == str or int)):
-					
-
-		# for k, v in data.items():
-		# 	print (k, type(v), list(data).index(k))
-		# 	if type(v) == list or dict:
-		# 		Emptylist.insert(list(data).index(k), k) # --> NEED TO MATCH THISD WITH ITS HEADER VALUE!! Because the indexes are correct (0, 1, 2, 3, 0, 1, 2, 3...), if needs to be sub of index 2, add 2 to index.
-		# 	# else:
-			# 	Emptylist.insert(list(data).index(k), list(data.keys())[list(data).index(k)])
-		# for k, v in data.items():
-		# 	return {k: headers_traverse(v)}
 		return {k: headers_traverse(v) for k, v in data.items()}
 
 	else:
@@ -111,8 +85,3 @@ def headers_traverse(data):
 headers_traverse(Examplelist)
 
 # -- END -- #
-
-print("--")
-print("")
-print(Emptylist)
-print (headers_basic(Examplelist))
